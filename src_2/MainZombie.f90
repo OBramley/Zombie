736f800b5a4639c8047a7c4f3b9c44cbdf93491a
Oliver Bramley <oliver.bramley@live.co.uk>
2022-11-15 21:19:44 +0000

    arc changes

--- src_2/MainZombie.f90
+++ src_2/MainZombie.f90
@@ -114,12 +114,13 @@ program MainZombie
                 call hamgen(haml,zstore,elect,ndet,1)
             else if(GPUflg.eq.'y')then
                 num_devices = omp_get_num_devices()
+                print*,num_devices
                 if(num_devices.eq.0)then 
                     max_threads=omp_get_max_threads()
+                    print*,max_threads
                     max_teams=1 !max_threads
                     threadpteam=max_threads
                 end if
-                
                 ! call omp_set_dynamic(.true.)
                 call hamgen_gpu(haml,zstore,elect,ndet,1)
             end if
