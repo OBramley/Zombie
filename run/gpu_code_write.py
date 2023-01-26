495d21aa001cf7e286be5febe0492e47c789ef04
Oliver Bramley <oliver.bramley@live.co.uk>
2023-01-26 16:56:13 +0000

    large progress on new hamiltonian routine

--- run/gpu_code_write.py
+++ run/gpu_code_write.py
@@ -1,5 +1,6 @@
 import inputs
 import numpy 
+import csv 
 
 choice=("j","k")
 alive="alive{0}({1})"
@@ -22,6 +23,9 @@ cran_neg="(-"+aa+")"
 cran_choice=(" ",cran,cran_neg)
 omp="!$omp task"
 ompend="!$omp end task"
+end="return"
+# el%h1ei("+str(i+1)+"
+
 
 def spatospin1(H1ea,norb):
     """Converting H1ea from spatial to spin"""
@@ -65,15 +69,20 @@ def one_elec_setup(norb,H1ei,occupancy_an_cr):
 
     h1=""
     h1count=0
- 
+    g=numpy.zeros(((norb*norb)))
 
     for i in range(norb): # i is annihilation 
         for j in range(norb): #j is creation
             if(H1ei[i,j]!=0.0):
+                g[h1count]=H1ei[i,j]
                 h1count=h1count+1
                 newline=0
+                if(h1count==1):
+                    srt="if(j.eq.1)then"
+                else:
+                    srt="else if(j.eq."+str(h1count)+") then"
                 if(i!=j):
-                    h1=h1+""+omp+" \n               "+"h1etot("+str(h1count)+")=el%h1ei("+str(i+1)+","+str(j+1)+")*"+an.format(str(i+1),choice[0],choice[1])+"*{0}".format(cr_choice[int(occupancy_an_cr[i,j,0,j])]).format(str(j+1),choice[0],choice[1])+"&\n               " 
+                    h1=h1+""+srt+" \n               "+"h1e=el*"+an.format(str(i+1),choice[0],choice[1])+"*{0}".format(cr_choice[int(occupancy_an_cr[i,j,0,j])]).format(str(j+1),choice[0],choice[1])+"&\n               " 
                     orb=2
                     for p in range(norb):
                         
@@ -88,9 +97,9 @@ def one_elec_setup(norb,H1ei,occupancy_an_cr):
                                     h1=h1+"&\n               "
                                     newline=0
                             else:
-                                h1=h1+"\n               "+ompend+"\n               "
+                                h1=h1+"\n               "+end+"\n       "
                 else:
-                    h1=h1+""+omp+" \n               "+"h1etot("+str(h1count)+")=el%h1ei("+str(i+1)+","+str(j+1)+")*"+cran.format(str(j+1),choice[0],choice[1])
+                    h1=h1+""+srt+" \n               "+"h1e=el*"+cran.format(str(j+1),choice[0],choice[1])
                     newline=1
                     orb=1
                     for p in range(norb):
@@ -105,16 +114,24 @@ def one_elec_setup(norb,H1ei,occupancy_an_cr):
                                     h1=h1+"&\n               "
                                     newline=0
                             else:
-                                h1=h1+"\n               "+ompend+"\n               "
+                                h1=h1+"\n               "+end+"\n       "
             else:
                 continue
     
-    return h1, h1count
+    with open("h1e.csv",'w', newline='')as csvfile:
+            spamwriter=csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow([h1count,' '])
+            for j in range(h1count):
+                spamwriter.writerow([g[j],' '])
+        
+    
+    return h1
 
 def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
    
     h2count=0
     h2=" "
+    g=numpy.zeros((norb*norb*norb*norb))
     for i in range(norb):
         for j in range(norb):
             if(i!=j):
@@ -122,14 +139,19 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                     for l in range(norb):
                         if(k!=l):
                             if(H2ei[i,j,k,l]!=0.0):
+                                g[h2count]=H2ei[i,j,k,l]
                                 h2count=h2count+1
                                 newline=0
                                 orb=0
+                                if(h2count==1):
+                                    srt="if(j.eq.1)then"
+                                else:
+                                    srt="else if(j.eq."+str(h2count)+") then"
                                 if(i!=k): #i≠k
                                     if(i!=l): #i≠k and i≠l
                                         if(j!=k): #i≠k and i≠l and j≠k
                                             if(j!=l): #i≠k and i≠l and j≠k j≠l i,j,k,l
-                                                h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+"*{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,i])]).format(str(i+1),choice[0],choice[1])+\
+                                                h2=h2+""+srt+" \n               "+"h2e=el*"+"{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,i])]).format(str(i+1),choice[0],choice[1])+\
                                                     "*{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,j])]).format(str(j+1),choice[0],choice[1])+"&\n               "
                                                 h2=h2+"*{0}".format(cr_choice[int(occupancy_2cr_2an[i,j,k,l,0,k])]).format(str(k+1),choice[0],choice[1])+"*{0}".format(cr_choice[int(occupancy_2cr_2an[i,j,k,l,0,l])]).format(str(l+1),choice[0],choice[1])+"&\n               "
                                                 orb=4
@@ -146,10 +168,10 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                                 h2=h2+"&\n               "
                                                                 newline=0
                                                         else:
-                                                            h2=h2+"\n               "+ompend+"\n               "
+                                                            h2=h2+"\n               "+end+"\n       "
 
                                             else:#i≠k and i≠l and j≠k j=l i,jcran,k
-                                                h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+"*{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,i])]).format(str(i+1),choice[0],choice[1])+\
+                                                h2=h2+""+srt+" \n               "+"h2e=el*"+"{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,i])]).format(str(i+1),choice[0],choice[1])+\
                                                     "*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,j])]).format(str(j+1),choice[0],choice[1])+"&\n               "
                                                 h2=h2+"*{0}".format(cr_choice[int(occupancy_2cr_2an[i,j,k,l,0,k])]).format(str(k+1),choice[0],choice[1])
                                                 orb=3
@@ -167,9 +189,9 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                                 h2=h2+"&\n               "
                                                                 newline=0
                                                         else:
-                                                            h2=h2+"\n               "+ompend+"\n               "
+                                                            h2=h2+"\n               "+end+"\n       "
                                         else: #i≠l and i≠k and j=k j≠l i, jcran, l
-                                            h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+"*{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,i])]).format(str(i+1),choice[0],choice[1])+\
+                                            h2=h2+""+srt+" \n               "+"h2e=el*"+"{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,i])]).format(str(i+1),choice[0],choice[1])+\
                                                 "*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,j])]).format(str(j+1),choice[0],choice[1])+"&\n               "
                                             h2=h2+"*{0}".format(cr_choice[int(occupancy_2cr_2an[i,j,k,l,0,l])]).format(str(l+1),choice[0],choice[1])
                                             newline=1
@@ -187,10 +209,10 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                             h2=h2+"&\n               "
                                                             newline=0
                                                     else:
-                                                        h2=h2+"\n               "+ompend+"\n               "
+                                                        h2=h2+"\n               "+end+"\n       "
                                     else:  #i≠k i=l
                                         if(j!=k): #i≠k and i=l and j≠k j≠l icran, j,k
-                                            h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+"*{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,j])]).format(str(j+1),choice[0],choice[1])+\
+                                            h2=h2+""+srt+" \n               "+"h2e=el*"+"{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,j])]).format(str(j+1),choice[0],choice[1])+\
                                                 "*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,i])]).format(str(i+1),choice[0],choice[1])+"&\n               "
                                             h2=h2+"*{0}".format(cr_choice[int(occupancy_2cr_2an[i,j,k,l,0,k])]).format(str(k+1),choice[0],choice[1])
                                             newline=1
@@ -207,9 +229,9 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                             h2=h2+"&\n               "
                                                             newline=0
                                                     else:
-                                                        h2=h2+"\n               "+ompend+"\n               "
+                                                        h2=h2+"\n               "+end+"\n       "
                                         else: #i≠k and i=l and j=k j≠l icran,jcran
-                                            h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+\
+                                            h2=h2+""+srt+" \n               "+"h2e=el*"+\
                                                 "*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,i])]).format(str(i+1),choice[0],choice[1])+"*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,j])]).format(str(j+1),choice[0],choice[1])+"&\n               "
                                             newline=0
                                             orb=2
@@ -225,10 +247,10 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                             h2=h2+"&\n               "
                                                             newline=0
                                                     else:
-                                                        h2=h2+"\n               "+ompend+"\n               "
+                                                        h2=h2+"\n               "+end+"\n       "
                                 else: #i=k i≠l
                                     if(j!=l): #i=k and i≠l j≠l and j≠k icran,j,l
-                                        h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+"*{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,j])]).format(str(j+1),choice[0],choice[1])+\
+                                        h2=h2+""+srt+" \n               "+"h2e=el*"+"{0}".format(an_choice[int(occupancy_2cr_2an[i,j,k,l,1,j])]).format(str(j+1),choice[0],choice[1])+\
                                             "*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,i])]).format(str(i+1),choice[0],choice[1])+"&\n               "+"*{0}".format(cr_choice[int(occupancy_2cr_2an[i,j,k,l,0,l])]).format(str(l+1),choice[0],choice[1])
                                         newline=1
                                         orb=3
@@ -244,11 +266,11 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                         h2=h2+"&\n               "
                                                         newline=0
                                                 else:
-                                                    h2=h2+"\n               "+ompend+"\n               "
+                                                    h2=h2+"\n               "+end+"\n       "
                                     
 
                                     else: #i=k and i≠l j=l and j≠k icran,jcran
-                                        h2=h2+""+omp+" \n               "+"h2etot("+str(h2count)+")=el%h2ei("+str(i+1)+","+str(j+1)+","+str(k+1)+","+str(l+1)+")"+\
+                                        h2=h2+""+srt+" \n               "+"h2e=el*"+\
                                             "*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,i])]).format(str(i+1),choice[0],choice[1])+"*{0}".format(cran_choice[int(occupancy_2cr_2an[i,j,k,l,0,j])]).format(str(j+1),choice[0],choice[1])+"&\n               "
                                         orb=2
                                         newline=0
@@ -264,7 +286,7 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
                                                         h2=h2+"&\n               "
                                                         newline=0
                                                 else:
-                                                    h2=h2+"\n               "+ompend+"\n               "
+                                                    h2=h2+"\n               "+end+"\n       "
                             else:
                                 continue
                         else:
@@ -272,7 +294,14 @@ def two_elec_setup(norb,H2ei,occupancy_2cr_2an):
             else:
                 continue
 
-    return h2, h2count
+    with open("h2e.csv",'w', newline='')as csvfile:
+            spamwriter=csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow([h2count,' '])
+            for j in range(h2count):
+                spamwriter.writerow([g[j],' '])
+    
+    
+    return h2
 
 
 
@@ -327,55 +356,40 @@ def ham_writer(h1e,eri_full):
     H1ei = spatospin1(h1e,norb)
     H2ei = spatospin2(eri_full,norb)
 
-    h1,h1count=one_elec_setup(norb,H1ei,occupancy_an_cr)
-    h2,h2count=two_elec_setup(norb,H2ei,occupancy_2cr_2an)
+    h1=one_elec_setup(norb,H1ei,occupancy_an_cr)
+    h2=two_elec_setup(norb,H2ei,occupancy_2cr_2an)
         
 
 
     with open('tester.f95','w',encoding="utf-8") as f:
-        f.write("MODULE ham2 \n")
+        f.write("MODULE ham_2 \n")
         f.write(" \n")
         f.write("   use globvars \n")
         f.write(" \n")
         f.write("   contains \n")
-        f.write(" \n")
-        f.write(" \n")
-        f.write("   subroutine ham_make(ham,z,elecs) \n")
-        f.write(" \n")
-        f.write("       implicit none\n")
-        f.write(" \n")
-        f.write("       type(hamiltonian), intent(inout)::ham \n")
-        f.write("       type(zombiest),dimension(:),intent(in)::z\n")
-        f.write("       type(elecintrgl),intent(in)::el \n")
-        f.write("       complex(kind=8),allocatable,dimension(:)::h1etot,h2etot \n")
-        f.write("       integer::j,k,ierr\n")
-        f.write(" \n")
-        f.write("       if (errorflag .ne. 0) return \n")
-        f.write(" \n")
-        f.write("       ierr=0\n")
-        f.write("       allocate(h1etot("+str(h1count)+"),stat=ierr)\n")
-        f.write("       allocate(h2etot("+str(h2count)+"),stat=ierr)\n")
-        f.write("       if (ierr/=0) then\n" )
-        f.write('           write(0,"(a,i0)") "Error in annihilation and creation array vector allocation. ierr had value ," ierr \n')
-        f.write("           errorflag=1\n")
-        f.write("           return\n")
+        f.write("\n")
+        f.write("   real(kind=8) function h1e(alive1,dead1,alive2,dead2,el,j)\n\n")
+        f.write("       implicit none\n\n")
+        f.write("       real(kind=8), dimension(:),intent(in)::alive1,dead1,alive2,dead2\n")
+        f.write("       real(kind=8),intent(in)::el\n\n")
+        f.write("       integer::j\n\n")
+        f.write("       "+h1+"\n\n")
         f.write("       end if\n")
+        f.write("     end function h1e\n")
+        f.write("\n\n")
+        f.write("   real(kind=8) function h2e(alive1,dead1,alive2,dead2,el,j)\n\n")
+        f.write("       implicit none\n\n")
+        f.write("       real(kind=8), dimension(:),intent(in)::alive1,dead1,alive2,dead2\n")
+        f.write("       real(kind=8),intent(in)::el\n\n")
+        f.write("       integer::j\n\n")
+        f.write("       "+h2+"\n\n")
+        f.write("       end if\n")
+        f.write("     end function h2e\n")
+
+
+
         f.write(" \n")
-        f.write("       do j=1,ndet\n")
-        f.write("           do k=j,ndet\n")
-        f.write("               "+h1+"\n")
-        f.write("               "+h2+"\n")
-        f.write("           end do\n")
-        f.write("       end do \n")
-        f.write(" \n")
-        f.write(" \n")
-        f.write(" \n")
-        f.write(" \n")
-        f.write(" \n")
-        f.write(" \n")
-        f.write("   end subroutine ham_make\n")
-        f.write(" \n")
-        f.write("END MODULE ham2 \n")
+        f.write("END MODULE ham_2 \n")
 
 
 # ham_writer()
