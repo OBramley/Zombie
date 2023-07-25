e1aeaf48837cf88e8fe1768d1e80e06c4338c19e
Oliver Bramley <oliver.bramley@live.co.uk>
2023-07-25 13:20:11 +0100

    added abilty to combine initially small basis sets into intermediate basis sets before combining into a final basis

--- run/combine.py
+++ run/combine.py
@@ -10,15 +10,9 @@ import sys
 import inputs
 import os
 import socket
-from glob import glob
-import getpass
 import shutil
 import subprocess
 import random
-import math 
-from pyscf import gto, scf, ao2mo
-from functools import reduce
-import numpy
 import csv
 
 # Checking input paramaters 
@@ -76,14 +70,41 @@ if os.path.exists("rundata.csv"):
 else:
   print("rundata.csv file does not exist")
 
+
+
+subnodes=4
 multflg=inputs.run['multiple']
-shutil.copy2(EXDIR+'/node_1/data/zombie_1001.csv',EXDIR+'/data')
+
+
+# If combining into temporary folders comment this all out if not using (for now) 
+folders=multflg/subnodes
+# Step 1
+# for l in range(1,subnodes):
+#     shutil.copy2(EXDIR+'/node_'+str(l+1)+'/data/zombie_1001.csv',EXDIR+'/node_'+str(l+1)+'_1/data/')
+#     cnt=2
+#     for i in range(int(folders*l),int((folders*l)+folders)):
+#         floc=EXDIR+'/node_'+str(i+1)+'/data/zombie_1'
+#         for j in range(2,inputs.zombs['ndet']+1):
+#             shutil.copy2(floc+"{:03d}".format(j)+".csv",EXDIR+'/node_'+str(l+1)+'_1/data/zombie_1'+"{:03d}".format(cnt)+".csv")
+#             cnt+=1
+
+
+# Step 2
+shutil.copy2(EXDIR+'/node_1_1/data/zombie_1001.csv',EXDIR+'/data')
 cnt=2
-for i in range(multflg):
-    floc=EXDIR+'/node_'+str(i+1)+'/data/zombie_1'
-    for j in range(2,inputs.zombs['ndet']+1):
-        shutil.copy2(floc+"{:03d}".format(j)+".csv",EXDIR+'/data/zombie_1'+"{:03d}".format(cnt)+".csv")
+for l in range(subnodes):
+    for j in range(2,int((folders*(inputs.zombs['ndet']-1))+2)):
+        shutil.copy2(EXDIR+'/node_'+str(l+1)+'_1/data/zombie_1'+"{:03d}".format(j)+".csv",EXDIR+'/data/zombie_1'+"{:03d}".format(cnt)+".csv")
         cnt+=1
+
+# # Single combination 
+# shutil.copy2(EXDIR+'/node_1/data/zombie_1001.csv',EXDIR+'/data')
+# cnt=2
+# for i in range(multflg):
+#     floc=EXDIR+'/node_'+str(i+1)+'/data/zombie_1'
+#     for j in range(2,inputs.zombs['ndet']+1):
+#         shutil.copy2(floc+"{:03d}".format(j)+".csv",EXDIR+'/data/zombie_1'+"{:03d}".format(cnt)+".csv")
+#         cnt+=1
 
 cnt=cnt-1
 
@@ -113,7 +134,9 @@ with open("rundata.csv",'w',newline='')as file:
     writer.writerow([zomstat,hamstat,inputs.run['imagprop'],inputs.run['beta'],inputs.run['timesteps'],inputs.run['clean'],inputs.run['gram'],inputs.run['gramnum'],inputs.run['grad'],'n'])
     writer.writerow([inputs.zombs['norb'],inputs.zombs['nel'],inputs.zombs['spin'],cnt,inputs.zombs['zomtyp'],inputs.zombs['rhf_1'],inputs.zombs['imagflg']])
     writer.writerow([inputs.run['hamfile'],inputs.run['ovrlfile'],inputs.run['cleanham']])
-   
+
+
+# Step 3
 os.environ["OMP_CANCELLATION"]="TRUE" 
 if(HPCFLG==1):
     if(inputs.run['cores']!=1):
@@ -131,12 +154,39 @@ if(HPCFLG==1):
     # f.write('time ./d_check.exe')
     f.write('time ./ZOMBIE.exe')
     f.close()
-    subprocess.call(['qsub',file1])
-else:
-    print(os.getcwd())
-    if(inputs.run['cores']!=1):
-        os.environ["OMP_NUM_THREADS"]=str(inputs.run['cores'])
-    subprocess.run(["./ZOMBIE.exe"])
+   
+for l in range(subnodes):
+    shutil.copy2(file1, EXDIR+'/node_'+str(l+1)+'_1')
+    shutil.copy2("rundata.csv", EXDIR+'/node_'+str(l+1)+'_1')
+    shutil.copy2("ZOMBIE.exe", EXDIR+'/node_'+str(l+1)+'_1')
+    os.chdir(EXDIR+'/node_'+str(l+1)+'_1')
+    command=['qsub',file1]
+    subprocess.call(command)
+
+   
+# os.environ["OMP_CANCELLATION"]="TRUE" 
+# if(HPCFLG==1):
+#     if(inputs.run['cores']!=1):
+#         os.environ["OMP_NUM_THREADS"]=str(inputs.run['cores'])
+#     number=random.randint(99999,1000000)
+#     file1="zombie"+str(number)+".sh"
+#     f=open(file1,"w")
+#     f.write("#$ -cwd -V \n")
+#     if(inputs.run['cores']!=1):
+#         f.write("#$ -pe smp "+str(inputs.run['cores'])+" \n") #Use shared memory parallel environemnt 
+#     f.write("#$ -l h_rt="+inputs.run['runtime']+"\n")
+#     f.write("#$ -l h_vmem=5G \n")
+#     f.write("export OMP_CANCELLATION=true \n")
+#     f.write("module add mkl \n")
+#     # f.write('time ./d_check.exe')
+#     f.write('time ./ZOMBIE.exe')
+#     f.close()
+#     subprocess.call(['qsub',file1])
+# else:
+#     print(os.getcwd())
+#     if(inputs.run['cores']!=1):
+#         os.environ["OMP_NUM_THREADS"]=str(inputs.run['cores'])
+#     subprocess.run(["./ZOMBIE.exe"])
         
         
 
