6f8cec1af85f98b2b5f2f6f78d139acdd36466bc
Oliver Bramley <oliver.bramley@live.co.uk>
2024-02-08 00:22:25 +0000

    mac

--- run/inputs.py
+++ run/inputs.py
@@ -14,14 +14,14 @@
 run={
     
     # What is the name of the run
-    'runfolder':'BH_stretch_newest_ham',
+    'runfolder':'Li2',
 
     # Amount of time to request on HPC 
     'runtime': "48:00:00",
 
     'nodes':1,
 
-    'cores':15,
+    'cores':8,
 
     'submissions':25,
 
@@ -124,22 +124,22 @@ run={
 #     'nel':6
 # }
 
-# pyscf={
-#      # The units the geometry of the molecule is set up in
-#     'units':'Angstrom',
-#      # The geometry of the molecule being investigated
-#     'atoms': 'Li 0 0 0; Li 0 0 2.673',
-#     # The type of basis used to generate the 1 and 2 electron integrals
-#     'bs' : 'cc-pVDZ',
-#     # How verbose do you want the PyScf output to be in your terminal?
-#     'verbosity' : 4,
-#     'symmetry' :True,
-#     'spin':0,
-#     'charge':0,
-#     'symmetry_subgroup' : 0, #0 is code for A1 point group
-#     'norb': 28,
-#     'nel':6
-# }
+pyscf={
+     # The units the geometry of the molecule is set up in
+    'units':'Angstrom',
+     # The geometry of the molecule being investigated
+    'atoms': 'Li 0 0 0; Li 0 0 2.673',
+    # The type of basis used to generate the 1 and 2 electron integrals
+    'bs' : 'cc-pVDZ',
+    # How verbose do you want the PyScf output to be in your terminal?
+    'verbosity' : 4,
+    'symmetry' :True,
+    'spin':0,
+    'charge':0,
+    'symmetry_subgroup' : 0, #0 is code for A1 point group
+    'norb': 28,
+    'nel':6
+}
 
 # pyscf={
 #      # The units the geometry of the molecule is set up in
@@ -222,22 +222,22 @@ run={
 #     'nel':8
 # }
 
-pyscf={
-     # The units the geometry of the molecule is set up in
-    'units':'Angstrom',
-     # The geometry of the molecule being investigated
-    'atoms': 'B 0 0 0; H 0 0 4.0',
-    # The type of basis used to generate the 1 and 2 electron integrals
-    'bs' : '6-31g**',
-    # How verbose do you want the PyScf output to be in your terminal?
-    'verbosity' : 4,
-    'symmetry' :True,
-    'spin':0,
-    'charge':0,
-    'symmetry_subgroup' : 0, #0 is code for A1 point group
-    'norb': 19,
-    'nel':6
-}
+# pyscf={
+#      # The units the geometry of the molecule is set up in
+#     'units':'Angstrom',
+#      # The geometry of the molecule being investigated
+#     'atoms': 'B 0 0 0; H 0 0 4.0',
+#     # The type of basis used to generate the 1 and 2 electron integrals
+#     'bs' : '6-31g**',
+#     # How verbose do you want the PyScf output to be in your terminal?
+#     'verbosity' : 4,
+#     'symmetry' :True,
+#     'spin':0,
+#     'charge':0,
+#     'symmetry_subgroup' : 0, #0 is code for A1 point group
+#     'norb': 19,
+#     'nel':6
+# }
 
 # pyscf={
 #      # The units the geometry of the molecule is set up in
