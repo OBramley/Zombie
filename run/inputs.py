f74f2a3fafcb71ba65884b0b6de2e46f1688db3e
Oliver Bramley <oliver.bramley@live.co.uk>
2024-02-08 17:45:19 +0000

    arc stuff

--- run/inputs.py
+++ run/inputs.py
@@ -14,14 +14,14 @@
 run={
     
     # What is the name of the run
-    'runfolder':'Li_test',
+    'runfolder':'BH_30_faster_ham',
 
     # Amount of time to request on HPC 
     'runtime': "48:00:00",
 
     'nodes':1,
 
-    'cores':8,
+    'cores':15,
 
     'submissions':25,
 
@@ -189,38 +189,38 @@ run={
 #     'nel':7
 # }
 
-# pyscf={
-#      # The units the geometry of the molecule is set up in
-#     'units':'atom',
-#      # The geometry of the molecule being investigated
-#     'atoms': 'F 0 0 0',
-#     # The type of basis used to generate the 1 and 2 electron integrals
-#     'bs' : 'cc-pVDZ',
-#     # How verbose do you want the PyScf output to be in your terminal?
-#     'verbosity' : 4,
-#     'symmetry' :True,
-#     'spin':1,
-#     'charge':0,
-#     'norb': 14,
-#     'nel':9
-# }
-
 pyscf={
      # The units the geometry of the molecule is set up in
-    'units':'Angstrom',
+    'units':'atom',
      # The geometry of the molecule being investigated
-    'atoms': 'Be 0 0 0; Be 0 0 2.45',
+    'atoms': 'F 0 0 0',
     # The type of basis used to generate the 1 and 2 electron integrals
     'bs' : 'cc-pVDZ',
     # How verbose do you want the PyScf output to be in your terminal?
     'verbosity' : 4,
     'symmetry' :True,
-    'spin':0,
+    'spin':1,
     'charge':0,
-    'symmetry_subgroup' : 0, #0 is code for A1 point group
-    'norb': 28,
-    'nel':8
+    'norb': 14,
+    'nel':9
 }
+
+# pyscf={
+#      # The units the geometry of the molecule is set up in
+#     'units':'Angstrom',
+#      # The geometry of the molecule being investigated
+#     'atoms': 'Be 0 0 0; Be 0 0 2.45',
+#     # The type of basis used to generate the 1 and 2 electron integrals
+#     'bs' : 'cc-pVDZ',
+#     # How verbose do you want the PyScf output to be in your terminal?
+#     'verbosity' : 4,
+#     'symmetry' :True,
+#     'spin':0,
+#     'charge':0,
+#     'symmetry_subgroup' : 0, #0 is code for A1 point group
+#     'norb': 28,
+#     'nel':8
+# }
 
 # pyscf={
 #      # The units the geometry of the molecule is set up in
@@ -239,22 +239,22 @@ pyscf={
 #     'nel':6
 # }
 
-# pyscf={
-#      # The units the geometry of the molecule is set up in
-#     'units':'Angstrom',
-#      # The geometry of the molecule being investigated
-#     'atoms': 'B 0 0 0; H 0 0 1.2324',
-#     # The type of basis used to generate the 1 and 2 electron integrals
-#     'bs' : '6-31g**',
-#     # How verbose do you want the PyScf output to be in your terminal?
-#     'verbosity' : 4,
-#     'symmetry' :True,
-#     'spin':0,
-#     'charge':0,
-#     'symmetry_subgroup' : 0, #0 is code for A1 point group
-#     'norb':19,
-#     'nel':6
-# }
+pyscf={
+     # The units the geometry of the molecule is set up in
+    'units':'Angstrom',
+     # The geometry of the molecule being investigated
+    'atoms': 'B 0 0 0; H 0 0 1.2324',
+    # The type of basis used to generate the 1 and 2 electron integrals
+    'bs' : '6-31g**',
+    # How verbose do you want the PyScf output to be in your terminal?
+    'verbosity' : 4,
+    'symmetry' :True,
+    'spin':0,
+    'charge':0,
+    'symmetry_subgroup' : 0, #0 is code for A1 point group
+    'norb':19,
+    'nel':6
+}
 
 # pyscf={
 #      # The units the geometry of the molecule is set up in
@@ -337,7 +337,7 @@ zombs={
     'spin':pyscf['spin'],
 
     # Number of Zombie states
-    'ndet':10,#pyscf['norb']*2, 
+    'ndet':200,#pyscf['norb']*2, 
 
     # Type of zombie states. Random (ran), Hartree Fock (HF) or biased (bb)
     'zomtyp':'bb',
--- src/gradient_descent.f90
+++ src/gradient_descent.f90
@@ -203,8 +203,8 @@ MODULE gradient_descent
             errorflag=1
             return
         end if 
-        strt=1
-        end=nel
+        strt=3
+        end=16
         chng_trk2=0 !stores which orbitals in the ZS have changed 
         rjct_cnt=0 !tracks how many rejections 
         acpt_cnt=0  !counts how many ZS have been changed
@@ -213,9 +213,9 @@ MODULE gradient_descent
         p=70-norb
         chng=0.0000001
         if(epoc_cnt.gt.2)then
-            chng_chng=250 !150
+            chng_chng=150
         else
-            chng_chng=400 !250
+            chng_chng=250
         end if
         lralt_zs=0
         lralt_extra=0
@@ -249,7 +249,7 @@ MODULE gradient_descent
                 pickerorb=scramble_norb(norb)
                 call haml_to_grad_do(haml,dvecs,thread)
 
-                do n=strt,end !(nel*3) !1,norb
+                do n=strt,end !nborstrt,end !(nel*3) !1,norb
                     pickorb=n !pickerorb(n)
                     call grad_calculate(haml,dvecs,zstore,grad_fin,pickorb)
                     thread%zom=zstore(pick)
@@ -305,7 +305,7 @@ MODULE gradient_descent
             end if 
 
             if(acpt_cnt_2.lt.(0.15*ndet))then 
-                ! lralt_extra=lralt_extra+1
+                lralt_extra=lralt_extra+1
                 
             end if 
            
@@ -314,25 +314,29 @@ MODULE gradient_descent
             chng_chng=chng_chng-1
             if(lralt_zs.gt.loop_max)then
                 lralt_zs=lralt_extra
-                end=end+4
-                strt=strt+4
-                if(end.gt.norb)then
-                    if(end.lt.norb+4)then
-                        end=norb
-                        strt=norb-nel
-                    else
-                        end=nel 
-                        strt=1
-                    end if
+                if(end.eq.norb)then
+                    end=16
+                    strt=3
+                    lralt_extra=0
                 end if
-                ! if((acpt_cnt_2.lt.((ndet)/3).or.((acpt_cnt_2.lt.3))))then
-                !     tracker=tracker+1
-                ! end if
+                if(modulo(epoc_cnt,4).eq.0)then 
+                    end=norb
+                    strt=1
+                    lralt_extra=0
+                end if 
+                if((acpt_cnt_2.lt.((ndet)/3).or.((acpt_cnt_2.lt.3))))then
+                    tracker=tracker+1
+                    end=norb
+                    strt=1
+                    lralt_extra=0
+                end if
             end if
 
 
-            if(((tracker.ge.1).or.(chng_chng.le.0)).and.(strt.eq.1))then!.and.(ndet.lt.10))then
+            if(((tracker.ge.2).or.(chng_chng.le.0)))then!.and.(strt.eq.1))then!.and.(ndet.lt.10))then
                 if(ndet.lt.300)then
+                        end=16
+                        strt=3
                         tracker=0
                         lralt_extra=0
                         loop_max_reset_cnt=0
@@ -370,7 +374,7 @@ MODULE gradient_descent
                             call zombiewriter(zstore(j),j,0)
                         end do
                         lralt_zs=0
-                        chng_chng=250 !150
+                        chng_chng=20 !150
                 else if(loop_max.lt.12)then
                     loop_max=loop_max+1
                     tracker=0
